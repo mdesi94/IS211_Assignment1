9315ba01b2cf83989a52c0e17314bf34a60380f6
mdesi94 <marissa.desimone1@gmail.com>
2022-01-29 17:52:46 -0500

    updated exception

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,30 @@
+# function to check whether numbers is divisible by divide
+def listDivide(numbers, divide = 2):
+    total = 0
+    for number in numbers:
+        if number % divide == 0:
+            total += 1
+    return total
 
-def listDivide(numbers, divide):
-    """
-    The function returns the number of elements in the numbers list that are divisibleby divide
-    """
+#create exception class
+class listDivideError(Exception):
     pass
 
+
 def testListDivide():
-    """
-    Test listDivide
-    """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
+#Test listDivide function
+    try:
+        listDivide([1,2,3,4,5])
+        listDivide([2,4,6,8,10])
+        listDivide([30,54,63,98,100], divide = 10)
+        listDivide([])
+        listDivide([1,2,3,4,5], 1)
+
+    except Exception:
+        raise listDivideError("cannot divide numbers list by divide value")
+
+
+
+
 if __name__ == "__main__":
     testListDivide()
